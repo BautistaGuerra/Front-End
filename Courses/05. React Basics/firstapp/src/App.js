7815b32ac630af0110df3950e9dfd46a9a68022d
Bautista Guerra <bauguerra@gmail.com>
2023-07-21 17:09:36 -0300

    React Basics week 1 completed
    React Basics week 1 completed

---
+++ Courses/05. React Basics/firstapp/src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+            <Heading firstName="Bob" />
+            <Heading firstName="John" />
+            <Heading firstName="Peter"/>
+    </div>
+  );
+}
+
+export default App;
